3620c5bf65e3b38ba73529762ff459b9edf3ef13
harry <hary50705@gmail.com>
2017-02-24 09:30:10 +0800

    20170224 UPDATE

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,7 +13,7 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
@@ -26,6 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+trainBeforeTime = time()
+clf.fit(features_train,labels_train)
+print("training time:", round(time()- trainBeforeTime, 3), "s")
+
+predictBeforeTime = time()
+pred = clf.predict(features_test)
+print("predict time:", round(time()- predictBeforeTime, 3), "s")
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels_test,pred))
 
 
 #########################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,8 +10,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
@@ -19,11 +18,34 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train = features_train[:len(features_train) // 100]
+#labels_train = labels_train[:len(labels_train) // 100]
 
 
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(C=10000,kernel='rbf')
+trainBeforeTime = time()
+clf.fit(features_train,labels_train)
+print("training time:", round(time()- trainBeforeTime, 3), "s")
+
+predictBeforeTime = time()
+pred=clf.predict(features_test)
+print("predict time:", round(time()- predictBeforeTime, 3), "s")
+
+from sklearn.metrics import accuracy_score
+
+print(accuracy_score(labels_test,pred))
+
+print("第10、26、50預測結果:",pred[10],',',pred[26],',',pred[50])
+
+count=0
+for predResult in pred:
+    if predResult == 1:
+        count+=1
+print("預測結果為“Chris” (1) 筆數:",count)
 
 #########################################################
 
